a4756bd29d28793f0117e2b8d630ade837652f42
Josh Weinstein <jweinst1@berkeley.edu>
2017-06-22 16:48:22 +0000

    C tok

--- exp/tokenizer.c
+++ exp/tokenizer.c
@@ -1,8 +1,18 @@
 #include <string.h>
 #include "tokenizer.h"
 
-void makeToken(const char* str, char* tokhold, bool* done){
-    if(!(*done)){
-        
+void makeToken(const char* str, char* tokhold, bool* done)
+{
+    if(!(*done))
+    {
+        while(*str)
+        {
+            switch(*str)
+            {
+                case '>':
+                    
+                    break;
+            }
+        }
     }
 }
