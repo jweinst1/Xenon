0df4c9c08c9242af6967d9b7c8e151f1f0623ed6
Josh Weinstein <jweinst1@berkeley.edu>
2017-06-27 19:54:31 +0000

    alloc start

--- src/head/xenonobj.h
+++ src/head/xenonobj.h
@@ -1,16 +1,23 @@
 #ifndef XENON_OBJECT_H
 #define XENON_OBJECT_H
 
+#include "allocator.h"
+
 
 
 //used for casting
-typedef unsigned char XenonType;
+typedef unsigned XenonType;
 
 
-
+//generic holder struct for raw void* data
 typedef struct {
     XenonType type;
+    void* data;
 } XenonObject;
+
+//constructor functions
+
+XenonObject cons_int_s(int value);
 
 
 
