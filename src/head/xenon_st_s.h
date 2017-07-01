c112703a873bc4bd3b5c85cf349bb75b3c12e4d5
Josh Weinstein <jweinst1@berkeley.edu>
2017-07-01 00:18:53 +0000

    xss funcs

--- src/head/xenon_st_s.h
+++ src/head/xenon_st_s.h
@@ -31,7 +31,9 @@ typedef struct  XenonStream_s XenonStream_s;
 //allocates on instance of xss
 inline XenonStream_s* xss_allo_s()
 {
-    return (XenonStream_s*)malloc(sizeof(XenonStream_s));
+    XenonStream_s* xss = (XenonStream_s*)malloc(sizeof(XenonStream_s));
+    xss->next = NULL;
+    return xss;
 }
 
 //frees one instance of xss
@@ -39,5 +41,40 @@ inline void xss_free_s(XenonStream_s* xss)
 {
     free(xss);
 }
+
+//pushes back one stream_s value
+inline XenonStream_s* xss_push(XenonStream_s* xss)
+{
+    XenonStream_s* newxss = xss_allo_s();
+    newxss->next = xss;
+    return newxss;
+}
+
+//connects 2 single streams
+inline void xss_connect(XenonStream_s* xss1, XenonStream_s* xss2)
+{
+    xss1->next = xss2;
+}
+
+//gets type
+inline unsigned char xss_type(XenonStream_s* xss)
+{
+    return xss->type;
+}
+
+inline int xss_next_null(XenonStream_s* xss)
+{
+    return xss->next == NULL;
+}
+
+/*SET GET functions*/
+
+inline void xss_set_char(XenonStream_s* xss, const char ch)
+{
+    xss->type = 0;
+    xss->data.i8 = ch;
+}
+
+
 
 #endif
