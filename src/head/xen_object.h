7ef0fa9b1832ec52863fc8607b60f9ae5e27ac50
Josh Weinstein <jweinst1@berkeley.edu>
2017-07-06 04:08:23 +0000

    more obj macro

--- src/head/xen_object.h
+++ src/head/xen_object.h
@@ -36,6 +36,30 @@ typedef struct XenObject XenObject;
 #define XenObject_CONNECT_PREV(xo1, xo2) do { \
                     xo1->prev = xo2; \
                     xo2->next = xo1; } while(0)
+                    
+#define XenObject_HAS_NEXT(xo) xo->next == NULL
+
+#define XenObject_HAS_PREV(xo) xo->prev == NULL
+
+#define XenObject_IS_END(xo) xo->next == NULL || xo->prev == NULL
+
+//advances pointer to back of stream
+#define XenObject_ADV_BACK(xo) while(xo->next != NULL) xo = xo->next
+
+//advances pointer to front of stream
+#define XenObject_ADV_FRONT(xo) while(xo->prev != NULL) xo = xo->prev
+
+#define XenObject_APPEND(xo1, xo2) do { \
+     while(xo1->next != NULL) xo1 = xo1->next; \
+     xo1->next = xo2; \
+     xo2->prev = xo1; \
+    }while(0)
+    
+#define XenObject_PUT(xo1, xo2) do { \
+     while(xo1->prev != NULL) xo1 = xo1->prev; \
+     xo1->prev = xo2; \
+     xo2->next = xo1; \
+    }while(0)
 
 
 #endif
