b43cac75725eaeb6e96edbbedfe922f4bc257a67
Josh Weinstein <jweinst1@berkeley.edu>
2017-08-12 18:16:28 +0000

    defining machine and statement system

--- src/head/statementtoken.h
+++ src/head/statementtoken.h
@@ -5,15 +5,26 @@
 extern "C" {
 #endif
 
+//types of xen tokens to be used
+typedef enum
+{
+    XenTokenType_Assign,
+    XenTokenType_Map,
+    XenTokenType_Filter
+} XenTokenType;
+
 //tokens consumed by machine to facilitate computation
 //allows them to be freely cast from one to another
 
-#define XenToken_HANDLE XenTokenType type;
+#define XenToken_HANDLE XenTokenType type; \
+                XenObject* buf;
 
 typedef struct
 {
     XenToken_HANDLE
 } XenToken;
+
+#define XenToken_GET_TYPE(xt) xt->type
 
 
 #ifdef __cplusplus
