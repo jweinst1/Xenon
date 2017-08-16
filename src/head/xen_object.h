aa7150d66b4720da74e455286fe40320dd56cad8
Josh Weinstein <jweinst1@berkeley.edu>
2017-08-16 18:38:04 +0000

    more refactoring of xenobject

--- src/head/xen_object.h
+++ src/head/xen_object.h
@@ -11,50 +11,95 @@
 class XenObject
 {
 public:
-    inline XenType getType()
+    XenType getType()
     {
         return type;
     }
     
-    inline void setType(XenType type)
+    void setType(XenType type)
     {
         type = type;
     }
     
-    inline XenObject* getNext()
+    XenObject* getNext()
     {
         return next;
     }
     
-    inline void setNext(XenObject* xo)
+    void setNext(XenObject* xo)
     {
         next = xo;
     }
     
-    inline XenObject* getPrev()
+    XenObject* getPrev()
     {
         return prev;
     }
     
-    inline void setPrev(XenObject* xo)
+    void setPrev(XenObject* xo)
     {
         prev = xo;
     }
     
-    inline bool nextIsNull()
+    bool nextIsNull()
     {
         return next == NULL;
     }
     
-    inline bool prevIsNull()
+    bool prevIsNull()
     {
         return prev == NULL;
     }
     
-    inline bool isSinglet()
+    bool isSinglet()
     {
         return prev == NULL && next == NULL;
     }
+    
+    inline void connectNext(XenObject* xo)
+    {
+        next = xo; 
+        xo->prev = this;
+    }
+    
+    inline void connectPrev(XenObject* xo)
+    {
+        prev = xo; 
+        xo->next = this;
+    }
+    
+    inline static void connect(XenObject* xo1, XenObject* xo2)
+    {
+        xo1->next = xo2;
+        xo2->prev = xo1;
+    }
+    //chain methods
+    inline XenObject* linkNext(XenObject* xo)
+    {
+        next = xo; 
+        xo->prev = this;
+        return xo;
+    }
+    
+    inline XenObject* linkPrev(XenObject* xo)
+    {
+        prev = xo; 
+        xo->next = this;
+        return xo;
+    }
+    
+    //gets reference to pointer
+    static XenObject& getRef(XenObject* xo)
+    {
+        return *xo;
+    }
+public:
+//iterator methods
+    XenObject* getFront();
+    XenObject* getBack();
+    
+    void append(XenObject* xo);
+    void put(XenObject* xo);
     
 private:
     XenType type;
@@ -64,53 +109,6 @@ private:
 
 
 
-#define XenObject_SET_NP_NULL(xo) do { \
-                   xo->next = NULL; \
-                   xo->prev = NULL;} while(0)
-                   
-#define XenObject_SET_N_NULL(xo) xo->next = NULL
-
-#define XenObject_SET_P_NULL(xo) xo->prev = NULL
-
-#define XenObject_IS_TYPE(xo, ty), xo->type == ty
-
-#define XenObject_GET_TYPE(xo) xo->type
-
-#define XenObject_SET_TYPE(xo, t) xo->type = t
-
-#define XenObject_SAME_TYPE(xo1, xo2) xo1->type == xo2->type
-
-#define XenObject_SAME_PNT(xo1, xo2) xo1 == xo2 
-
-#define XenObject_CAST(xo) (XenObject*)xo
-
-#define XenObject_CONNECT_NEXT(xo1, xo2) do { \
-                    xo1->next = xo2; \
-                    xo2->prev = xo1; } while(0)
-                    
-#define XenObject_CONNECT_PREV(xo1, xo2) do { \
-                    xo1->prev = xo2; \
-                    xo2->next = xo1; } while(0)
-                    
-#define XenObject_HAS_NEXT(xo) xo->next == NULL
-
-#define XenObject_HAS_PREV(xo) xo->prev == NULL
-
-#define XenObject_GET_NEXT(xo) xo->next
-
-#define XenObject_GET_PREV(xo) xo->prev
-
-#define XenObject_GET_NEXT_NEXT(xo) xo->next->next
-
-#define XenObject_GET_PREV_PREV(xo) xo->prev->prev
-
-#define XenObject_IS_END(xo) xo->next == NULL || xo->prev == NULL
-
-#define XenObject_IS_SING(xo) xo->next == NULL && xo->prev == NULL
-
-#define XenObject_IS_FRONT(xo) xo->prev == NULL
-
-#define XenObject_IS_BACK(xo) xo->next == NULL
 
 //advances pointer to back of stream
 #define XenObject_ADV_BACK(xo) while(xo->next != NULL) xo = xo->next
