ef1f26cbb565a141064e6996306ffe409b33f3bc
Joshua Weinstein <jweinstein@splunk.com>
2017-10-05 16:59:48 -0700

    tok changes

--- src/parser/Tokenizer.cpp
+++ src/parser/Tokenizer.cpp
@@ -1,4 +1,5 @@
 #include "Tokenizer.h"
+#include "Lexer.h"
 
 
 Tokenizer::Tokenizer(Parser* prs) : _state(TokzState_Base), _parser(prs)
@@ -9,9 +10,31 @@ Tokenizer::Tokenizer(Parser* prs) : _state(TokzState_Base), _parser(prs)
 void Tokenizer::tokenize(char* code)
 {
         char* cPnt = code;
-        switch(_state)
+        XenEventType* tokenEvent = nullptr;
+        while (*cPnt)
         {
-        case TokzState_Base:
-                if(*cPnt == '#') _state = TokzState_Comment;
+                switch(_state)
+                {
+                case TokzState_Base:
+                        if(TokenSets::eventName.contains(*cPnt))
+                        {
+                                Lexer::eventType(cPnt, tokenEvent, &cPnt);
+                                if(tokenEvent)
+                                {
+
+                                }
+                        }
+                        else if(*cPnt == '#')
+                        {
+                                _state = TokzState_Comment;
+                                cPnt++;
+                        }
+                case TokzState_Comment:
+                        if(*cPnt == '\n')
+                        {
+                                _state = TokzState_Base;
+                                cPnt++;
+                        }
+                }
         }
 }
